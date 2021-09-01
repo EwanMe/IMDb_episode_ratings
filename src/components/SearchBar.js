58f8a9a8b437d9e978ccf10c5d1746f5edfddd30
Hallvard Jensen <hallvard.jensen@gmail.com>
2021-09-01 22:46:13 +0200

    remove: unnecessary div in SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,22 +1,20 @@
 const SearchBar = (props) => {
   return (
-    <div>
-      <input
-        type="search"
-        name="search-bar"
-        placeholder="Search for TV series"
-        style={{ width: '100%', height: '3em' }}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter') {
-            props.search(e.target.value);
-            e.target.value = '';
-          }
-        }}
-        onKeyUp={(e) => {
-          props.update(e.target.value);
-        }}
-      />
-    </div>
+    <input
+      type="search"
+      name="search-bar"
+      placeholder="Search for TV series"
+      style={{ width: '100%', height: '3em' }}
+      onKeyDown={(e) => {
+        if (e.key === 'Enter') {
+          props.search(e.target.value);
+          e.target.value = '';
+        }
+      }}
+      onKeyUp={(e) => {
+        props.update(e.target.value);
+      }}
+    />
   );
 };
 
