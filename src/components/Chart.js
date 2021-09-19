e28d2bad831f7ae32b13ba0fb4790691cb55dae8
Hallvard Jensen <hallvard.jensen@gmail.com>
2021-09-19 23:54:56 +0200

    change: data plots are shown/hidden on demand
    * all season data loaded first time and modified later without unloading
    * axis max/min modified properly by api
    * small changes to tooltip layout

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,67 +1,99 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import c3 from 'c3';
 import 'c3/c3.css';
 
 const Chart = (props) => {
-  useEffect(() => {
-    if (props.data.Episodes) {
-      renderChart(updateChartData(props.data));
-    }
-  }, [props.data, props.isDynamic]);
+  const [chart, setChart] = useState(null);
 
-  const updateChartData = (data) => {
+  useEffect(() => {
+    if (props.isLoaded) {
+      const allPlots = props.data.map((item) => createDataPlot(item));
+      renderChart(allPlots);
+    }
+  }, [props.isLoaded]);
+
+  useEffect(() => {
+    // TODO: Load new data in first time, and hide them after, not unload.
+    // console.log('Chart.js 17: ', props.selection);
+    if (chart) {
+      const loaded = chart.data();
+
+      loaded.forEach((item) => {
+        if (props.selection.includes(item.id)) {
+          chart.show(item.id);
+        } else {
+          chart.hide(item.id);
+        }
+      });
+    }
+  }, [props.selection, chart]);
+
+  useEffect(() => {
+    if (chart) {
+      if (props.isDynamic) {
+        chart.axis.max(undefined);
+        chart.axis.min(undefined);
+      } else {
+        chart.axis.range({ max: { y: 10 }, min: { y: 1 } });
+      }
+    }
+  }, [props.isDynamic]);
+
+  const createDataPlot = (data) => {
     const ratings = data.Episodes.map((ep) => ep.imdbRating);
-    ratings.unshift('Season ' + data.Season);
+    ratings.unshift('Season ' + data.Season); // Data array starts with label name
     return ratings;
   };
 
-  const renderChart = (data) => {
-    c3.generate({
-      bindto: '#chart',
-      unload: true,
-      data: {
-        type: 'line',
-        columns: [data],
-      },
-      axis: {
-        y: {
-          label: {
-            text: 'Rating',
-            position: 'outer-middle',
-          },
-          max: props.isDynamic ? undefined : 10,
-          min: props.isDynamic ? undefined : 1,
+  const renderChart = (plots) => {
+    setChart(
+      c3.generate({
+        bindto: '#chart',
+        unload: true,
+        data: {
+          type: 'line',
+          columns: plots,
         },
-        x: {
-          label: {
-            text: 'Episodes',
-            position: 'inner-center',
+        axis: {
+          y: {
+            label: {
+              text: 'Rating',
+              position: 'outer-middle',
+            },
+            max: props.isDynamic ? undefined : 10,
+            min: props.isDynamic ? undefined : 1,
           },
-          tick: {
-            format: (x) => x + 1, // x-axis starts on episode 1
+          x: {
+            label: {
+              text: 'Episodes',
+              position: 'inner-center',
+            },
+            tick: {
+              format: (x) => x + 1, // x-axis starts on episode 1
+            },
           },
         },
-      },
-      color: {
-        pattern: [
-          '#478EFF',
-          '#FF5353',
-          '#7AFF60',
-          '#FFAE4B',
-          '#7157FF',
-          '#FAFF5B',
-          '#43EFFF',
-          '#FF68FF',
-        ],
-      },
-      tooltip: {
-        format: {
-          title: (d) => props.data.Episodes[d].Title,
-          name: (name, ratio, id, index) => name,
-          value: (name, ratio, id, index) => 'Rating: ' + name,
+        color: {
+          pattern: [
+            '#478EFF',
+            '#FF5353',
+            '#7AFF60',
+            '#FFAE4B',
+            '#7157FF',
+            '#FAFF5B',
+            '#43EFFF',
+            '#FF68FF',
+          ],
         },
-      },
-    });
+        tooltip: {
+          format: {
+            //title: (d) => props.data.Episodes[d].Title,
+            name: (name) => name,
+            value: (name) => 'Rating: ' + name,
+          },
+        },
+      })
+    );
   };
 
   if (props.error) {
