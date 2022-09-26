fc5538871ed0cd5fd2bf4b8fe44ed6b593897da8
Hallvard Jensen <hallvard.jensen@gmail.com>
2022-09-26 21:24:51 +0200

    reverted mistaken fix to searchbar, to be fixed in #11

--- src/components/Search/UserSearch.js
+++ src/components/Search/UserSearch.js
@@ -51,7 +51,7 @@ const UserSearch = ({ getShow, setNoResults }) => {
       <SearchIcon className="search-icon" />
       <SearchBar
         autoSelect={() => {
-          if (items[0] && showQuery) {
+          if (items[0]) {
             setShowQuery(items[0].imdbID); // Default value is first item.
             setShowAutocomplete(false);
           } else {
