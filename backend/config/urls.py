b751469e3515a7437f4a290b63404fb3e95c4c28
Hallvard Jensen <hallvard.jensen@gmail.com>
2023-06-05 23:38:49 +0200

    add management command to populate database
    - remove populate_database as an api endpoint
    - clean up docstrings and type info

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -5,12 +5,6 @@ from imdb import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(
-        "download/<str:table>/",
-        views.populate_database,
-        name="download_single",
-    ),
-    path("download/", views.populate_database),
     path("show/<str:id>/", views.get_show),
     path("ratings/<str:id>/", views.get_episode_ratings),
     path("search/", views.get_free_search),
