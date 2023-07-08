892921802ad44001c59277750aff043e5f624401
Hallvard Jensen <hallvard.jensen@gmail.com>
2023-07-09 00:42:34 +0200

    Improved mobile responsiveness of website

--- frontend/src/components/ShowInfo/ShowCard.js
+++ frontend/src/components/ShowInfo/ShowCard.js
@@ -1,7 +1,7 @@
 import Chip from './Chip';
 import RatingBox from './RatingBox';
 
-const ShowInfo = ({
+const ShowCard = ({
   title,
   poster,
   year,
@@ -14,27 +14,29 @@ const ShowInfo = ({
   return (
     <div className="show-card-wrapper">
       <img src={poster} alt={`${title} poster`} className="show-poster" />
-      <div className="show-info">
-        <div className="show-header">
-          <h1 className="show-title">{title}</h1>
-          <RatingBox rating={rating} />
-        </div>
+      <div className="show-header">
+        <h1 className="show-title">{title}</h1>
+        <RatingBox rating={rating} />
         <p className="show-year">({year})</p>
+      </div>
+      <div className="show-body">
         <p className="plot-summary">{plot}</p>
-        <div className="show-roles-wrapper">
-          <p className="show-roles">
+        <div className="show-role-wrapper">
+          <p className="show-role">
             <b>Written by:&ensp;</b> {writers}
           </p>
-          <p className="show-roles">
+          <p className="show-role">
             <b>Starring:&emsp;&ensp;</b> {actors}
           </p>
         </div>
-        {genre.split(',').map((item) => (
-          <Chip key={item} text={item.trim()} />
-        ))}
+        <div className="chip-wrapper">
+          {genre.split(',').map((item) => (
+            <Chip key={item} text={item.trim()} />
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
-export default ShowInfo;
+export default ShowCard;
