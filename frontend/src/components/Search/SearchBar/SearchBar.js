0d6e77c25d1f61de5535e04082cd8b394d5c30f2
Hallvard Jensen <hallvard.jensen@gmail.com>
2023-08-10 17:41:18 +0200

    add button to clear search
    * tidy: move search icon inside search bar component

--- frontend/src/components/Search/SearchBar/SearchBar.js
+++ frontend/src/components/Search/SearchBar/SearchBar.js
@@ -1,27 +1,37 @@
+import SearchIcon from '@mui/icons-material/Search';
+import MdCancel from '@mui/icons-material/Cancel';
+
 const SearchBar = ({ update, autoSelect }) => {
   return (
-    <input
-      className="search-bar"
-      type="search"
-      name="search-bar"
-      placeholder="Search for TV series"
-      onKeyDown={(e) => {
-        if (e.key === 'Enter') {
-          autoSelect(e.target.value); // Automatically selects most relevant show.
-          e.target.value = '';
-        }
-      }}
-      onKeyUp={(e) => {
-        if (e.key !== 'Enter') {
-          update(e.target.value);
-        }
-      }}
-      onClick={(e) => {
-        if (e.target.value.length !== 0) {
-          update(e.target.value);
-        }
-      }}
-    />
+    <>
+      <SearchIcon className="search-icon" />
+      <button className="cancel-search-icon">
+        <MdCancel
+          onClick={() => (document.querySelector('.search-bar').value = '')}
+        />
+      </button>
+      <input
+        className="search-bar"
+        type="search"
+        name="search-bar"
+        placeholder="Search for TV series"
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            autoSelect(e.target.value); // Automatically selects most relevant show.
+          }
+        }}
+        onKeyUp={(e) => {
+          if (e.key !== 'Enter') {
+            update(e.target.value);
+          }
+        }}
+        onClick={(e) => {
+          if (e.target.value.length !== 0) {
+            update(e.target.value);
+          }
+        }}
+      />
+    </>
   );
 };
 
